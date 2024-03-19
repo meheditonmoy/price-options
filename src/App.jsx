8f9caca67e90dbd62272d271645963867443b693
Tonmoy <meheditonmoy2024@gmail.com>
2024-03-19 23:16:53 +0600

    added loding spiner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import LineChart from './components/LineChart/LineChart'
 import NavBar from './components/NavBar/NavBar'
+import Phones from './components/Phones/Phones'
 import PriceOption from './components/priceOption/PriceOption'
 // import DaisyNav from './components/DaisyNav/DaisyNav'
 
@@ -15,6 +16,7 @@ function App() {
       {/* <h1 className='text-7xl'>Vite + React</h1> */}
       <PriceOption></PriceOption>
       <LineChart></LineChart>
+      <Phones></Phones>
     </>
   )
 }
---
+++ src/components/Phones/Phones.jsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { Audio } from 'react-loader-spinner';
+
+
+
+const Phones = () => {
+    const [phones, setPhones] = useState([]);
+    const [loding, setLoding] = useState(true);
+
+    useEffect(() => {
+        // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+        // .then(res => res.json())
+        // .then(data  => setPhones(data.data))
+
+        axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
+            .then(data => {
+                const phoneData = data.data.data;
+                const phoneWithFakeData = phoneData.map(phone => {
+                    const obj = {
+                        name: phone.phone_name,
+                        price: parseInt(phone.slug.split('-')[1])
+                    }
+                    return obj;
+                })
+                console.log(phoneWithFakeData);
+                setPhones(phoneWithFakeData)
+                setLoding(false)
+            });
+    }, [])
+    return (
+        <div>
+            {loding && <Audio
+                height="80"
+                width="80"
+                radius="9"
+                color="green"
+                ariaLabel="three-dots-loading"
+                wrapperStyle
+                wrapperClass
+            />}
+            <h2 className="text-5xl">Phones: {phones.length}</h2>
+            <BarChart width={800} height={300} data={phones}>
+                <Bar dataKey="price" fill="#8884d8" />
+                <XAxis dataKey="name"></XAxis>
+                <YAxis></YAxis>
+                <Tooltip></Tooltip>
+            </BarChart>
+        </div>
+    );
+};
+
+export default Phones;
